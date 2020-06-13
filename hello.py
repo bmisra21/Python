946e32cab0ed190f5cd47f479f29a3207508e25a
bmisra21 <teetu2003@gmail.com>
2020-06-13 18:40:22 -0400

    6/13

--- hello.py
+++ hello.py
@@ -1,8 +1,37 @@
-num1 = input("Type in one number: ")
-num2 = input("Type in another number: ")
+import csv
+import pandas as pd
+import numpy as np
+import os
+import os.path
+from os import path
 
-print(str(float(num1)+float(num2)))
+#create array for output (RDGP/capita)
+filename = "API_NY.GDP.PCAP.CD_DS2_en_csv_v2_1120951.csv"
 
-numbers = [1,2,3,4,5]
-for num in numbers:
-    print(num)
+df = pd.read_csv(filename, skiprows=4)
+print (df['2018'][1])
+output = np.array(df['2018'])
+
+
+#create vectors for inputs
+row_count = sum(1 for row in "API_NY.GDP.PCAP.CD_DS2_en_csv_v2_1120951.csv")
+dimensions = (row_count-5,1)
+inputs = np.ones(dimensions)
+path = 'C:\\Users\\bmisr\\VSCodeFiles\\python'
+folder = os.fsencode(path)
+
+filenames = []   
+for file in os.listdir(folder):
+    filename = os.fsdecode(file)
+    if filename.endswith( ('.csv') ): 
+        filenames.append(filename)
+for name in filenames:
+    print(name)
+    if os.path.isfile(name):
+        print ("File exist")
+    else:
+        print ("File not exist")
+    
+    
+     
+    
